bb102d8e9afb8e7b9f6b5d8690709c8fae552ae3
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2016-09-30 16:53:33 +0100

    Added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,9 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute,hashHistory} from 'react-router';
 
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 import Main from './components/Main';
 import Weather from './components/Weather';
 import About from './components/About';
