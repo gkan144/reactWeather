0bf3fb308455841b1e3584b0874ac0f1e8d24b57
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2016-10-03 11:41:05 +0100

    Corrected error modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+import ReactDomServer from 'react-dom/server';
 
 class ErrorModal extends React.Component {
 
   componentDidMount() {
+    var {title, message} = this.props;
+    var modalMarkup = <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+      <h4>{title}</h4>
+      <p>{message}</p>
+      <p><button className="button hollow" data-close="">Okay</button></p>
+    </div>;
+
+    var $modal = $(ReactDomServer.renderToString(modalMarkup));
+    $(ReactDom.findDOMNode(this)).html($modal);
+
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   }
 
   render() {
-    var {title, message} = this.props;
-    return <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-      <h4>{title}</h4>
-      <p>{message}</p>
-      <p><button className="button hollow" data-close="">Okay</button></p>
-    </div>
+    return <div></div>
   }
 }
 
