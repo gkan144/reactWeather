e5097dc50a6d50692dbc75404e1533a2d4f37532
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2016-10-03 10:05:14 +0100

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 var About = (props) => {
-    return <h3>About</h3>;
+  return <div>
+    <h1 className="text-center">About</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus faucibus risus. In auctor nisi quis sodales porta. Curabitur et varius arcu. Vestibulum tempus efficitur venenatis. Etiam fermentum ornare condimentum. Morbi ullamcorper velit ipsum. Nulla ut dictum mauris. Aliquam a.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus faucibus risus. In auctor nisi quis sodales porta. Curabitur et varius arcu. Vestibulum tempus efficitur venenatis. Etiam fermentum ornare condimentum. Morbi ullamcorper velit ipsum. Nulla ut dictum mauris. Aliquam a.
+    </p>
+    <ul>
+      <li><a href="https://facebook.github.io/react">React</a></li>
+      <li><a href="http://openweathermap.org">Open Weather Map</a></li>
+    </ul>
+  </div>;
 };
 
 export default About;
